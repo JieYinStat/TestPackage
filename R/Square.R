c7081e3c28c593d8bc399f6e75c41e3e6f46f7ec
JieYinStat <yjabcdhot@hotmail.com>
2024-04-01 14:37:44 +0800

    A test commit after configurate token

--- R/Square.R
+++ R/Square.R
@@ -25,3 +25,17 @@ Square <- function(x) {
 Square_plus_1 <- function(x) {
  x^2 + 1
 }
+
+#' Test Function: Square_plus_2
+#'
+#' @param x a vector.
+#'
+#' @return a vector.
+#' @export
+#'
+#' @examples
+#' x <- 1:3
+#' Square_plus_1(x)
+Square_plus_2 <- function(x) {
+  x^2 + 2
+}
