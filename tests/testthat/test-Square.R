423078e8eda9d5d3e4bb197b6b0ca752057b7803
JieYinStat <yjabcdhot@hotmail.com>
2024-04-01 14:58:54 +0800

    Add test

--- tests/testthat/test-Square.R
+++ tests/testthat/test-Square.R
@@ -1,3 +1,9 @@
 test_that("Square works", {
   expect_equal(Square(1:3), c(1, 4, 9))
 })
+test_that("Square_plus_1 works", {
+  expect_equal(Square_plus_1(1:3), c(2, 5, 10))
+})
+test_that("Square_plus_2 works", {
+  expect_equal(Square_plus_2(1:3), c(3, 6, 11))
+})
