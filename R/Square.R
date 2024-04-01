5260239c67559d6542bd0808c4a12911fc7f957b
JieYinStat <yjabcdhot@hotmail.com>
2024-04-01 14:09:59 +0800

    Initial configuration

---
+++ R/Square.R
@@ -0,0 +1,27 @@
+#' Test Function: Square.
+#'
+#' @param x a vector.
+#'
+#' @return a vector.
+#' @export
+#'
+#' @examples
+#' x <- 1:3
+#' Square(x)
+Square <- function(x) {
+  x^2
+}
+
+#' Test Function: Square_plus_1
+#'
+#' @param x a vector.
+#'
+#' @return a vector.
+#' @export
+#'
+#' @examples
+#' x <- 1:3
+#' Square_plus_1(x)
+Square_plus_1 <- function(x) {
+ x^2 + 1
+}
---
+++ tests/testthat/test-Square.R
@@ -0,0 +1,3 @@
+test_that("Square works", {
+  expect_equal(Square(1:3), c(1, 4, 9))
+})
